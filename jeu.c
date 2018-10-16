40e67d53e468acc93495909df34aad70bdfce9ac
TMachon <TMachon@users.noreply.github.com>
2018-10-16 13:33:28 +0200

    Add files via upload

--- jeu.c
+++ jeu.c
@@ -37,36 +37,50 @@ char key_pressed() {
 
 int main (int argc, char * argv[]) {
 
-	char c;
+	char c, direction="N", canon;
+	int changement = 0;
+	char fichier[] = "tanks/tank2_N_N.txt";
 	while (( c = key_pressed()) != 'e') {
 		if (c == 'd') {
-			char * fichier = "tanks/tank1_E_E.txt";
-			int taille = taille_fichier(fichier);
-			char texte[taille];
-			lecture_fichier(fichier, texte, taille);
-			affichage( texte , taille);
+			fichier[12] = 'E';
+			changement = 1;
 		}
 		if (c == 'q') {
-			char * fichier = "tanks/tank1_O_O.txt";
-			int taille = taille_fichier(fichier);
-			char texte[taille];
-			lecture_fichier(fichier, texte, taille);
-			affichage( texte , taille);
+			fichier[12] = 'O';
+			changement = 1;
 		}
 		if (c == 's') {
-			char * fichier = "tanks/tank1_S_S.txt";
-			int taille = taille_fichier(fichier);
-			char texte[taille];
-			lecture_fichier(fichier, texte, taille);
-			affichage( texte , taille);
+			fichier[12] = 'S';
+			changement = 1;
 		}
 		if (c == 'z') {
-			char * fichier = "tanks/tank1_N_N.txt";
+			fichier[12] = 'N';
+			changement = 1;
+		}
+		if (c == 'm') {
+			fichier[14] = 'E';
+			changement = 1;
+		}
+		if (c == 'k') {
+			fichier[14] = 'O';
+			changement = 1;
+		}
+		if (c == 'l') {
+			fichier[14] = 'S';
+			changement = 1;
+		}
+		if (c == 'o') {
+			fichier[14] = 'N';
+			changement = 1;
+		}
+		if (changement) {
+			changement = 0;
 			int taille = taille_fichier(fichier);
 			char texte[taille];
 			lecture_fichier(fichier, texte, taille);
-			affichage( texte , taille);
+			affichage(texte , taille);
 		}
+		
 	}
 
 }
